da643564a9d3e6e1b31da3a2814e6ddd8c50e0c8
Armian Sangaji <iansangaji@engineer.com>
2019-04-30 15:11:34 +0900

    Create send.php

---
+++ send.php
@@ -0,0 +1,14 @@
+<?php
+$username	= "your_username";
+$number		= readline("Number  : ");
+$pesan		= readline("Message : ");
+$sendmess	= json_decode(file_get_contents("https://api.iansangaji.co/sms/sms.php?pesan=".urlencode($pesan)."&no=".$number."&user=".$username));
+$success = $sendmess->output->message;
+if($success == "success"){
+	echo "=====================\n";
+	echo "Status  : ".$sendmess->output->message."\n";
+}else{
+	echo $sendmess->output->message."\n";
+	exit();
+}
+?>
